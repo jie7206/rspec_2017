ba29b842221979fde98676b3e114966063bd0e6f
jie7206 <1524556052@qq.com>
2019-09-02 16:45:30 +0800

    finish ch5 homework

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -60,6 +60,51 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 
+  describe "#edit" do
+    context "as an authorized user" do
+      before do
+        @user = FactoryGirl.create(:user)
+        @project = FactoryGirl.create(:project, owner: @user)
+      end
+      it "responds successfully" do
+        sign_in @user
+        get :edit, params: { id: @project.id }
+        expect(response).to be_success
+      end
+    end
+    context "as an unauthorized user" do
+      before do
+        @user = FactoryGirl.create(:user)
+        other_user = FactoryGirl.create(:user)
+        @project = FactoryGirl.create(:project, owner: other_user)
+      end
+      it "redirects to the dashboard" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  describe "#new" do
+    context "as an authorized user" do
+      before do
+        @user = FactoryGirl.create(:user)
+      end
+      it "responds successfully" do
+        sign_in @user
+        get :new
+        expect(response).to be_success
+      end
+    end
+    context "as an unauthorized user" do
+      it "redirects to the dashboard" do
+        get :new
+        expect(response).to redirect_to "/users/sign_in"
+      end
+    end
+  end
+
   describe "#create" do
 
     context "as an authenticated user" do
